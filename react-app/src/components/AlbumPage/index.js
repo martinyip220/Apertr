0728a71a5ff95f2dd81f1e3860a4713d2e77c11f
Martin Yip <martinyip220@gmail.com>
2023-02-03 16:05:58 -0800

    fixed deletion of first item of album bug, now displays content in empty album pg

--- react-app/src/components/AlbumPage/index.js
+++ react-app/src/components/AlbumPage/index.js
@@ -8,6 +8,7 @@ import "./index.css";
 
 function AlbumPage() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { albumId } = useParams();
   const [loaded, setLoaded] = useState(false);
   const id = parseInt(albumId);
@@ -32,31 +33,42 @@ function AlbumPage() {
     dispatch(getOneAlbumThunk(id));
   }, [dispatch]);
 
+  const handleRedirect = async (id) => {
+    await dispatch(getOneAlbumThunk(id));
+
+    return history.push(`/albums/${id}/edit`);
+  };
+
   if (!loaded) return null;
-  if (!photos || photos.length < 1) return null;
+  if (!photos) return null;
 
   return (
     <div className="album-pg-whole">
-      <div style={{position: "relative"}}>
-      <img
-        className="album-banner"
-          src={photos[0]?.photoImg}
+      <div style={{ position: "relative" }}>
+        <img
+          className="album-banner"
+          src={
+            photos.length
+              ? photos[0]?.photoImg
+              : "https://previews.123rf.com/images/imagecatalogue/imagecatalogue1611/imagecatalogue161115996/66632914-no-se-encuentra-el-texto-sello-de-goma-sello-de-marca-de-agua-t%C3%ADtulo-dentro-de-banner-rectangular-re.jpg"
+          }
           onError={addDefaultSrc}
-        alt="album-banner"
-      ></img>
-      <div className="album-pg-background-top">
-        <div className="album-banner-title-descr">
-          <div className="album-banner-info-title">{currentAlbum.title}</div>
-          <div className="album-banner-info-description">
-            {currentAlbum.description}
+          alt="album-banner"
+        ></img>
+        <div className="album-pg-background-top">
+          <div className="album-banner-title-descr">
+            <div className="album-banner-info-title">{currentAlbum.title}</div>
+            <div className="album-banner-info-description">
+              {currentAlbum.description}
+            </div>
           </div>
+
+          <div className="album-banner-info-credits">By: {owner}</div>
         </div>
-
-        <div className="album-banner-info-credits">By: {owner}</div>
-      </div>
       </div>
 
-      <div className="album-photos-btm-ctn">
+      {photos.length >= 1 && (
+        <div className="album-photos-btm-ctn">
         {photos &&
           photos.map((photo) => (
             <div key={photo.id}>
@@ -69,6 +81,16 @@ function AlbumPage() {
             </div>
           ))}
       </div>
+      )}
+      {!photos.length && (
+        <div className="empty-album-ctn">
+          <div className="empty-album-msg">This album is empty. Please add some photos here!</div>
+          <i
+            className="fa-solid fa-pen-to-square add-to-empty-alb-btn"
+            onClick={() => handleRedirect(currentAlbum.id)}
+          ></i>
+        </div>
+      )}
     </div>
   );
 }
