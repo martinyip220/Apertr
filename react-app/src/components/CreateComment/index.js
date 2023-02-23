b4654ac42229a0b4d01d106375b73922478eee50
Martin Yip <martinyip220@gmail.com>
2023-02-23 15:16:48 -0800

    styled comment text area with additional counter of chars as error handling

--- react-app/src/components/CreateComment/index.js
+++ react-app/src/components/CreateComment/index.js
@@ -2,48 +2,67 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
-import { createCommentThunk, getAllPhotoCommentsThunk } from "../../store/comment";
+import {
+  createCommentThunk,
+  getAllPhotoCommentsThunk,
+} from "../../store/comment";
 import "./index.css";
 
-
 function CommentForm({ photoId }) {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [comment, setComment] = useState("")
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [comment, setComment] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const commentEle = document.getElementById("comment");
+  const counterEle = document.getElementById("counter");
 
-        const commentData = {
-            comment,
-            photoId
-        }
+  commentEle?.addEventListener("input", function (e) {
+    const target = e.target;
 
-        await dispatch(createCommentThunk(commentData));
-        await dispatch(getAllPhotoCommentsThunk(photoId));
+    // Get the `maxlength` attribute
+    const maxLength = target.getAttribute("maxlength");
 
-        setComment("")
+    // Count the current number of characters
+    const currentLength = target.value.length;
 
-        history.push(`/photos/${photoId}`);
-    }
+    counterEle.innerHTML = `${currentLength}/${maxLength}`;
+  });
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    placeholder="Add a comment"
-                    type="text"
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                    required
-                />
-                <button type="submit">
-                    Comment
-                </button>
-            </form>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const commentData = {
+      comment,
+      photoId,
+    };
+
+    await dispatch(createCommentThunk(commentData));
+    await dispatch(getAllPhotoCommentsThunk(photoId));
+
+    setComment("");
+
+    history.push(`/photos/${photoId}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className="textarea-comment"
+          maxLength="200"
+          id="comment"
+          placeholder="Add a comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          required
+        />
+        <div className="under-comment-info">
+          <div className="counter-char" id="counter"></div>
+          <button className="comment-submit-btn" type="submit">Comment</button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
-
 
 export default CommentForm;
